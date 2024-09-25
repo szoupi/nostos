d5dd00f2b5865c1a185330d69a1d7d2f45631729
szoupi <szoupi@gmail.com>
2024-09-25 17:22:57 +0300

    add header patterns

--- patterns/header-search-button-ERROR-NOT-DISPLAYED.php
+++ patterns/header-search-button-ERROR-NOT-DISPLAYED.php
@@ -15,21 +15,16 @@
 <div class="wp-block-group alignfull"><!-- wp:group {"tagName":"header","metadata":{"categories":["woo-commerce"],"patternName":"woocommerce-blocks/header-essential"},"align":"full","className":"tt-nostos-header","style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"40px","right":"40px"},"margin":{"bottom":"var:preset|spacing|80"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <header class="wp-block-group alignfull tt-nostos-header" style="margin-bottom:var(--wp--preset--spacing--80);padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:group {"style":{"position":{"type":""}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:site-logo /-->
-
 <!-- wp:group {"style":{"spacing":{"blockGap":"4px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group"><!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}}} /-->
-
 <!-- wp:site-tagline {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"xs"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
-
 <!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
 <div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 <div class="wp-block-group"><!-- wp:navigation {"ref":74,"style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","justifyContent":"right"}} /-->
-
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 <div class="wp-block-group"><!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-only","buttonUseIcon":true,"isSearchFieldHidden":true,"style":{"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"color":{"background":"#fffefe"}},"textColor":"contrast","fontSize":"xs"} /-->
-
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
 <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Discover</a></div>
